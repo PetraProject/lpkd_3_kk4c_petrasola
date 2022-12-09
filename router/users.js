38ed71119001b89507c8f91694012b584278a5f1
PetraProject <114985909+PetraProject@users.noreply.github.com>
2022-12-08 22:13:09 -0800

    ulangan harian kk4c

---
+++ router/users.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const usercontrollers = require('../controllers/users')
+
+
+
+  router.get('/users', usercontrollers.index)
+
+  router.get('/user/:id', usercontrollers.show)
+
+  router.post('/user', usercontrollers.store)
+
+  router.put('/user/:id', usercontrollers.update)
+
+  router.delete('/user/:id', usercontrollers.delete) 
+
+  module.exports = router
